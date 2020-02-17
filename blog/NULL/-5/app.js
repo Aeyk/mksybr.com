5550b3bf68c6d6a3ada5f2cf12aece6f39c48f39
Malik Kennedy <mksybr@gmail.com>
2020-02-17 15:59:17 -0800

    Split hex string into 4 bytes: opcodes

--- blog/NULL/-5/app.js
+++ blog/NULL/-5/app.js
@@ -19,7 +19,7 @@ var key = new Array(16);
 
 
 let hello_world_rom = '';
-
+let hello_world_code = '';
 var oReq = new XMLHttpRequest();
 oReq.open("GET", "./helloworld.rom", true);
 oReq.responseType = "arraybuffer";
@@ -37,11 +37,47 @@ oReq.onload = function(oEvent) {
         hello_world_rom +=
             ('00' + byteArray[byt].toString(16)).slice(-2) || '';
     }
-    console.log(hello_world_rom.toString().match(/.{1,4}/g));
-
+    hello_world_code = hello_world_rom.toString().match(/.{1,4}/g).toArray();
+    console.log(hello_world_code);
 };
 
 oReq.send(null)
+
+
+
+1
+/* OPCODES FOR HELLO WORLD
+0: "6278"  6XNN	Const	Vx = NN	Sets VX to NN.
+1: "a500"  ANNN	MEM	I = NNN	Sets I to the address NNN.
+2: "6301"  6XNN
+3: "6401"  6XNN
+4: "f10a"
+5: "00e0"  0NNN	Call		Calls RCA 1802 program at address NNN. 
+                            Not necessary for most ROMs.
+6: "f218"  FX18	Sound	sound_timer(Vx)	    Sets the sound timer to VX.
+7: "f129"  FX29	MEM	I=sprite_addr[Vx]   Sets I to the location of the 
+                                        sprite for the character in VX. 
+                                        Characters 0-F (in hexadecimal) 
+                                        are represented by a 4x5 font.
+8: "d345" DXYN	Disp	draw(Vx,Vy,N)	    Draws a sprite at coordinate 
+                                        (VX, VY) that has a width of 
+                                        8 pixels and a height of N pixels. 
+                                        Each row of 8 pixels is read as 
+                                        bit-coded starting from memory 
+                                        location I; I value doesn’t change
+                                        after the execution of this 
+                                        instruction. As described above, 
+                                        VF is set to 1 if any screen pixels
+                                        are flipped from set to unset when 
+                                        the sprite is drawn, and to 0 if 
+                                        that doesn’t happen.
+9: "1200"
+*/
+
+
+
+
+
 
 // function open_hello_world() {
 //     let fr = new FileReader();
