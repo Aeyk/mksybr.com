ef780678478e5e9fa3eb9a9646bc20c0558b8f18
Malik Kennedy <mksybr@gmail.com>
2020-02-17 16:07:22 -0800

    Begin Opcode Lookup for Mov 6XNN

--- blog/NULL/-5/app.js
+++ blog/NULL/-5/app.js
@@ -45,40 +45,6 @@ oReq.send(null)
 
 
 
-1
-/* OPCODES FOR HELLO WORLD
-0: "6278"  6XNN	Const	Vx = NN	Sets VX to NN.
-1: "a500"  ANNN	MEM	I = NNN	Sets I to the address NNN.
-2: "6301"  6XNN
-3: "6401"  6XNN
-4: "f10a"
-5: "00e0"  0NNN	Call		Calls RCA 1802 program at address NNN. 
-                            Not necessary for most ROMs.
-6: "f218"  FX18	Sound	sound_timer(Vx)	    Sets the sound timer to VX.
-7: "f129"  FX29	MEM	I=sprite_addr[Vx]   Sets I to the location of the 
-                                        sprite for the character in VX. 
-                                        Characters 0-F (in hexadecimal) 
-                                        are represented by a 4x5 font.
-8: "d345" DXYN	Disp	draw(Vx,Vy,N)	    Draws a sprite at coordinate 
-                                        (VX, VY) that has a width of 
-                                        8 pixels and a height of N pixels. 
-                                        Each row of 8 pixels is read as 
-                                        bit-coded starting from memory 
-                                        location I; I value doesn’t change
-                                        after the execution of this 
-                                        instruction. As described above, 
-                                        VF is set to 1 if any screen pixels
-                                        are flipped from set to unset when 
-                                        the sprite is drawn, and to 0 if 
-                                        that doesn’t happen.
-9: "1200"
-*/
-
-
-
-
-
-
 // function open_hello_world() {
 //     let fr = new FileReader();
 //     console.log(fr.readAsBinaryString("helloworld.rom"));
@@ -114,12 +80,59 @@ function main_loop() {
 }
 
 
+function Chip8.() {
+
+}
+/* OPCODES FOR HELLO WORLD
+   0: "6278"  6XNN	Const	Vx = NN	Sets VX to NN. i.e sets 27 to 78.
+   1: "a500"  ANNN	MEM	I = NNN	Sets I to the address NNN.
+   2: "6301"  6XNN
+   3: "6401"  6XNN
+   4: "f10a"
+   5: "00e0"  0NNN	Call		Calls RCA 1802 program at address NNN. 
+   Not necessary for most ROMs.
+   6: "f218"  FX18	Sound	sound_timer(Vx)	    Sets the sound timer to VX.
+   7: "f129"  FX29	MEM	I=sprite_addr[Vx]   Sets I to the location of the 
+   sprite for the character in VX. 
+   Characters 0-F (in hexadecimal) 
+   are represented by a 4x5 font.
+   8: "d345" DXYN	Disp	draw(Vx,Vy,N)	    Draws a sprite at coordinate 
+   (VX, VY) that has a width of 
+   8 pixels and a height of N pixels. 
+   Each row of 8 pixels is read as 
+   bit-coded starting from memory 
+   location I; I value doesn’t change
+   after the execution of this 
+   instruction. As described above, 
+   VF is set to 1 if any screen pixels
+   are flipped from set to unset when 
+   the sprite is drawn, and to 0 if 
+   that doesn’t happen.
+   9: "1200"
+*/
+
+
+
+
+
+
 var Chip8 = {
+    memory: new Array(4096),
     init: function() { },
     emulateSingleCycle: function() {
         // Lookup Opcode
         // Decode Opcode
         // Execute Opcode
         // Update Timers
+    },
+    lookupOpcode: function(op) {
+        if (op.length !== 4) {
+            switch (op[0]) {
+                case '6':
+                    for (nibs in op) {
+                        return op[2].toNumber();
+                    }
+            }
+        }
     }
 };
